116a7d06b7094a52bf6feea31a42d27ba4ad6ab2
Emma Odametey <emma.odametey@turntabl.io>
2023-03-23 17:23:21 +0000

    lab for pointers array added

---
+++ fxns_prg_struct/q2.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+# include "q2.h"
+
+void funct(int number){
+    std::cout << number;
+}
+void funct(char letter){
+    std::cout << letter;
+}
+
+void funct(float number){
+    std::cout << number;
+}
+
+void funct(long number){
+    std::cout << number;
+}
+
+void funct(std::string sentence){
+    std::cout << sentence;
+}
